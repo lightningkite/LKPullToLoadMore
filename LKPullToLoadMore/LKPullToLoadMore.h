5ca73dadd6977e77d0843b86e2a8aa3cfcd64c55
Erik Sargent <erik@lightningkite.com>
2015-05-04 17:39:19 -0600

    Initial Commit

---
+++ LKPullToLoadMore/LKPullToLoadMore.h
@@ -0,0 +1,19 @@
+//
+//  LKPullToLoadMore.h
+//  LKPullToLoadMore
+//
+//  Created by Erik Sargent on 5/4/15.
+//  Copyright (c) 2015 Lightning Kite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LKPullToLoadMore.
+FOUNDATION_EXPORT double LKPullToLoadMoreVersionNumber;
+
+//! Project version string for LKPullToLoadMore.
+FOUNDATION_EXPORT const unsigned char LKPullToLoadMoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LKPullToLoadMore/PublicHeader.h>
+
+
